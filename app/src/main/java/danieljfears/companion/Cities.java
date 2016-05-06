fffb2bc2dcf26c8b34e78faa93b6d336c0188005
Daniel Fears <danieljfears@gmail.com>
2016-05-06 13:40:08 +0100

    Initial commit with working nav

---
+++ app/src/main/java/danieljfears/companion/Cities.java
@@ -0,0 +1,107 @@
+package danieljfears.companion;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Cities extends AppCompatActivity {
+
+    //Firebase
+    Firebase mRootRef;
+    ArrayList<String> mMessages = new ArrayList<>();
+
+    //UI
+    TextView mTextView;
+    ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cities);
+
+        Firebase.setAndroidContext(this);
+
+        mRootRef = new Firebase("https://danieljfears.firebaseio.com/");
+
+        mTextView = (TextView)findViewById(R.id.textView);
+        mListView = (ListView)findViewById(R.id.listView);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        final ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mMessages);
+
+        mListView.setAdapter(adapter);
+
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+
+            public void onItemClick(AdapterView<?> arg0, View v, int position, long arg3)
+            {
+                String selectedCity=mMessages.get(position);
+                Toast.makeText(getApplicationContext(), "Choice : "+selectedCity,   Toast.LENGTH_LONG).show();
+
+                //Intent i= new Intent(MainActivity.this,secondActivity.class);
+                //i.putExtra("string",mMessages.get(position);
+                //startActivity(i);
+                //finish();
+
+            }
+        });
+
+        Firebase messagesRef = mRootRef.child("City");
+        messagesRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String message = dataSnapshot.getKey();
+                Log.v("E_VALUE", message);
+                mMessages.add(message);
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/danieljfears/companion/MainActivity.java
@@ -0,0 +1,60 @@
+package danieljfears.companion;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    //
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        Button buttongps = (Button) findViewById(R.id.buttongps);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+           public void onClick(View v) {
+
+                Intent i= new Intent(MainActivity.this,Cities.class);
+                startActivity(i);
+
+            }
+        });
+
+        buttongps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "Button pressed",   Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+}
