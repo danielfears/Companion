b72b919f61a3138e967214233219b5942dd96a75
danieljfears <fea05095797@student.citybathcoll.ac.uk>
2016-05-07 12:27:47 +0100

    GPS working, can get and compare distances

--- app/src/main/java/danieljfears/companion/MainActivity.java
+++ app/src/main/java/danieljfears/companion/MainActivity.java
@@ -1,6 +1,7 @@
 package danieljfears.companion;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,6 +26,8 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     GPSTracker gps;
+    public static double latitude;
+    public static double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,23 +52,58 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                gps = new GPSTracker(MainActivity.this);
+                    GetLocation();
 
-                //Toast.makeText(getApplicationContext(), "Find user location",   Toast.LENGTH_LONG).show();
 
-                if(gps.canGetLocation()) {
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
 
-                    Toast.makeText(
-                            getApplicationContext(),
-                            "Your Location is -\nLat: " + latitude + "\nLong: "
-                                    + longitude, Toast.LENGTH_LONG).show();
-                } else {
-                    gps.showSettingsAlert();
-                }
+                    //double distance=Math.sqrt((bathlat-latitude)*(latitude-bathlat))
+
             }
         });
+
+    }
+
+    public void GetLocation() {
+
+            gps = new GPSTracker(MainActivity.this);
+
+            if(gps.canGetLocation()) {
+                latitude = gps.getLatitude();
+                longitude = gps.getLongitude();
+
+            } else {
+                gps.showSettingsAlert();
+            }
+
+        double bathlat = 51.375801;
+        double bathlong = -2.359904;
+
+                    /*Toast.makeText(
+                            getApplicationContext(),
+                            "Your Location is -\nLat: " + latitude + "\nLong: "
+                                    + longitude, Toast.LENGTH_LONG).show(); */
+
+        Location loc1 = new Location("");
+        loc1.setLatitude(latitude);
+        loc1.setLongitude(longitude);
+
+        Location loc2 = new Location("");
+        loc2.setLatitude(bathlat);
+        loc2.setLongitude(bathlong);
+
+        float distanceInMeters = loc1.distanceTo(loc2);
+
+        // Converts to miles
+        //distanceInMeters = ((distanceInMeters / 1000) / 8) * 5;
+        int roundedDistance = Math.round(distanceInMeters);
+
+        System.out.println(latitude);
+        System.out.println(longitude);
+        System.out.println(roundedDistance);
+
+        Toast.makeText(
+                getApplicationContext(),
+                "Distance is: " + roundedDistance, Toast.LENGTH_LONG).show();
 
     }
 
